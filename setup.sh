1cb02ba7ac220b88487b62110a662b31d6a55ad1
Seth Berrier <berriers@uwstout.edu>
2026-06-04 17:59:57 -0500

    Updates to setup script for DSM

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+
+# Check if the script is running with root/sudo privileges
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run with sudo or as root!" >&2
+  exit 1
+fi
+
 echo "Building base SVN image..."
 docker build -t svn-base -f subversion/Dockerfile.svn-base subversion/
 
-echo "Starting services..."
+echo "Ensure empty bind dirs exist..."
+if [ ! -d "./traefik/logs" ]; then
+  mkdir "./traefik/logs"
+fi
+
+if [ ! -d "./portainer/portainer_data" ]; then
+  mkdir "./portainer/portainer_data"
+fi
+
+echo "Starting compose services..."
 docker compose up -d --build
